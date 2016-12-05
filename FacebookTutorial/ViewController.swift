345659ac428d48153c10e28759524c45b7e0b53a
Raymundo Peralta <movile@peta.mx>
2016-12-05 13:36:23 -0600

    funciona el login y la comparticion con facebook

--- FacebookTutorial/ViewController.swift
+++ FacebookTutorial/ViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var botonLogin: FBSDKLoginButton!
+    
+    let contentURL = "http://avaluos.peta.mx"
+    let contentURLImage = "http://peta.mx/images/favicon/apple-icon-180x180.png"
+    let contentTitle = "Está usando InmobiliaApp para lograr un buen trato para su nueva casa"
+    let contentDescription = "esta localización le gusta y está a buen precio "
+    var imagePicker = UIImagePickerController()
+    var mediaSelected = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
@@ -27,6 +37,7 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
             loginView.readPermissions = ["public_profile", "email", "user_friends"]
             loginView.delegate = self
             self.returnUserData()
+            self.showShareButtons()
         }
         else
         {
@@ -43,7 +54,7 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     // Facebook Delegate Methods
     
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        print("User Logged In")
+        print("We are in loginButton <----------------")
         
         if ((error) != nil)
         {
@@ -56,18 +67,19 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         }
         else {
             print("Entramos por el succed")
+            print("We are logged in")
             // If you ask for multiple permissions at once, you
             // should check if specific permissions missing
             if result.grantedPermissions.contains("email")
             {
-                // Do work
+                // Do work with the email
                 print("Veamos del result del succeed")
                 print("granted \(result.grantedPermissions)")
                 print("declained \(result.declinedPermissions)")
-                //print(result.dictionaryWithValuesForKeys(["email"]))
             }
             print("Vamos a ver el user data")
             self.returnUserData()
+            self.showShareButtons()
         }
         
     }
@@ -95,6 +107,14 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
                 print("User Name is: \(userName)")
                 let picture : NSString = result.valueForKey("picture")?.valueForKey("data")?.valueForKey("url") as! NSString
                 print("User Picture URL is: \(picture)")
+                // picture contiene realmente el URL de donde hay que descargar la imagen
+                if let url = NSURL(string: picture as String) {
+                    if let data = NSData(contentsOfURL: url) {
+                        self.ImageView.contentMode = UIViewContentMode.ScaleAspectFit
+                        self.ImageView.image = UIImage(data: data)
+                    }        
+                }
+
                 let email : NSString = result.valueForKey("email") as! NSString
                 print("User Email: \(email)")
                 let verified : CFBoolean = result.valueForKey("verified") as! CFBoolean
@@ -107,5 +127,120 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func showShareButtons()
+    {
+        self.showLinkButton()
+        self.showPhotoButton()
+        self.showVideoButton()
+    }
+    
+    // Link Methods
+    
+    func showLinkButton()
+    {
+        let content : FBSDKShareLinkContent = FBSDKShareLinkContent()
+        content.contentURL = NSURL(string: self.contentURL)
+        content.contentTitle = self.contentTitle
+        content.contentDescription = self.contentDescription
+        content.imageURL = NSURL(string: self.contentURLImage)
+        
+        let button : FBSDKShareButton = FBSDKShareButton()
+        button.shareContent = content
+        button.frame = CGRectMake((UIScreen.mainScreen().bounds.width - 100) * 0.5, 50, 100, 25)
+        self.view.addSubview(button)
+        
+        let label : UILabel = UILabel()
+        label.frame = CGRectMake((UIScreen.mainScreen().bounds.width - 200) * 0.5, 25, 200, 25)
+        label.text = "Link Example"
+        label.textAlignment = .Center
+        self.view.addSubview(label)
+    }
+    
+    // Photo Methods
+    
+    func showPhotoButton()
+    {
+        let button : UIButton = UIButton()
+        button.backgroundColor = UIColor.blueColor()
+        button.setTitle("Choose Photo", forState: .Normal)
+        button.frame = CGRectMake((UIScreen.mainScreen().bounds.width - 150) * 0.5, 125, 150, 25)
+        button.addTarget(self, action: #selector(ViewController.photoBtnClicked), forControlEvents: .TouchUpInside)
+        self.view.addSubview(button)
+        
+        let label : UILabel = UILabel()
+        label.frame = CGRectMake((UIScreen.mainScreen().bounds.width - 200) * 0.5, 100, 200, 25)
+        label.text = "Photos Example"
+        label.textAlignment = .Center
+        self.view.addSubview(label)
+    }
+    
+    func photoBtnClicked(){
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.SavedPhotosAlbum){
+            print("Photo capture")
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum;
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+            
+            self.mediaSelected = "Photo"
+        }
+        
+    }
+    
+    // Video Methods
+    
+    func showVideoButton()
+    {
+        let button : UIButton = UIButton()
+        button.backgroundColor = UIColor.blueColor()
+        button.setTitle("Choose Video", forState: .Normal)
+        button.frame = CGRectMake((UIScreen.mainScreen().bounds.width - 150) * 0.5, 200, 150, 25)
+        button.addTarget(self, action: #selector(ViewController.videoBtnClicked), forControlEvents: .TouchUpInside)
+        self.view.addSubview(button)
+        
+        let label : UILabel = UILabel()
+        label.frame = CGRectMake((UIScreen.mainScreen().bounds.width - 200) * 0.5, 175, 200, 25)
+        label.text = "Video Example"
+        label.textAlignment = .Center
+        self.view.addSubview(label)
+    }
+    
+    func videoBtnClicked(){
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.SavedPhotosAlbum){
+            print("Video capture")
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+            
+            self.mediaSelected = "Video"
+        }
+        
+    }
+    
+    // Used for Both Photo & Video Methods
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        if self.mediaSelected == "Photo"
+        {
+            let photo : FBSDKSharePhoto = FBSDKSharePhoto()
+            photo.image = info[UIImagePickerControllerOriginalImage] as! UIImage
+            photo.userGenerated = true
+            let content : FBSDKSharePhotoContent = FBSDKSharePhotoContent()
+            content.photos = [photo]
+        }
+        
+        if self.mediaSelected == "Video"
+        {
+            let video : FBSDKShareVideo = FBSDKShareVideo()
+            video.videoURL = info[UIImagePickerControllerMediaURL] as! NSURL
+            let content : FBSDKShareVideoContent = FBSDKShareVideoContent()
+            content.video = video
+        }
+        
+        self.mediaSelected = ""
+    }
+    
 }
 
